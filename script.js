fae1d6b12cfb52d7a87081dd8de050985e565be4
Drake Gooding <dgooding1008@gmail.com>
2021-06-15 21:49:53 -0500

    Basic HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,60 @@
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+var forcastEl = document.querySelector('#repcontainer');
+var searchTerm = document.querySelector('#repo-search-term');
+
+
+
+
+
+let loc = document.getElementById("location");
+let tempValue = document.getElementById("temperature");
+let humidity = document.getElementById("humidity");
+let windSpeed = document.getElementById("wind-speed")
+let uv = document.getElementById("uv")
+let climate = document.getElementById("climate")
+var searchTermEl = document.getElementById("search")
+ console.log("hi")
+
+
+ var formSubmitHandler = function(event) {
+     event.preventDefault();
+
+     var searchTerm = searchTermEl.value.trim();
+
+     if (searchTerm) {
+         getCity(searchTerm);
+
+         loc.textContent = '';
+         searchTermEl.value = '';
+     } else {
+         alert('Please enter a valid City')
+     }
+ };
+
+ var getCity = function (name) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&appid=2ac382e894c8dfd8170abc00caca0a7f';
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayCity(data, name);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to OpenWeatherAPI');
+      });
+
+      loc.textContent = (data.name)
+      tempValue.textContent = 'Temperature: '(data.main.temp)
+      humidity.textContent = 'Humidity: '(data.main.humidity)
+      windSpeed.textContent = 'Windspeed: '(data.wind.speed)
+      uv.textContent = "UV"
+  };
+
+  document.getElementById("searchbtn").addEventListener('click', formSubmitHandler);
+    
